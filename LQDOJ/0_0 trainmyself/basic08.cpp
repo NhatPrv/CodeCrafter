52afbe3040888a6d46f28d6493e4148273c8d57c
NhatPrv <torikun2005@gmail.com>
2024-11-18 21:37:14 +0700

    basic08

---
+++ LQDOJ/0_0 trainmyself/basic08.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+// code by #CodeCrafters_Nholl (danglongnhat)
+
+int main ()  {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    char c; cin >> c;
+    cout << (char)((int)(c)+31);
+
+    return 0;
+}
