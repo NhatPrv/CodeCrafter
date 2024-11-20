9c8e157c7a8379aa42295259ea209848924655a9
NhatPrv <torikun2005@gmail.com>
2024-11-20 18:32:01 +0700

    cdl1p10.cpp

---
+++ LQDOJ/0_0 trainmyself/cdl1p10.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+// code by #CodeCrafters_Nholl (danglongnhat)
+
+int main ()  {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;    cin >> n;
+    cout << n * (n+1) / 2;
+
+    return 0;
+}
