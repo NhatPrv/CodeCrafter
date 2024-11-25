a734799793ff04b411ee3ad0c7aeaa954a528ba0
NhatPrv <torikun2005@gmail.com>
2024-11-25 23:12:00 +0700

    cdl2p6.cpp

---
+++ LQDOJ/0_0 trainmyself/cdl2p6.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+// code by #CodeCrafters_Nholl (danglongnhat)
+
+int main ()  {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;  cin >> n;
+    if (n >= 18) cout << "Ban da du tuoi bau cu ";
+    else cout << "Ban chua den tuoi bau cu ";
+
+    return 0;
+}
