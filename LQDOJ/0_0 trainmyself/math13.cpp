3e20b07b122087dd8a19c4ab89f2785a92e8370e
NhatPrv <torikun2005@gmail.com>
2024-11-27 09:47:22 +0700

    math13.cpp

---
+++ LQDOJ/0_0 trainmyself/math13.cpp
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+// code by #CodeCrafters_Nholl (danglongnhat)
+
+int main ()  {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long a, b; cin >> a >> b;
+    cout << a / b;
+
+    return 0;
+}
