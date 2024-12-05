f1e40057a4c215ae2afb3ff06d95de2cb5eb303b
NhatPrv <torikun2005@gmail.com>
2024-12-05 18:59:40 +0700

    cdl2p3.cpp

---
+++ LQDOJ/0_0 trainmyself/cdl2p3.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+// code by #CodeCrafters_Nholl (danglongnhat)
+
+int main ()  {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long a, b, c;  cin >> a >> b >> c;
+    if (a > 0 && b > 0 && c > 0) cout << "Ca ba so deu duong";
+    else cout << "It nhat mot so am";
+
+    return 0;
+}
